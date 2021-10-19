bff726acbd42eb890a9e8811613d71b2f22bc61c
chaitanya <chaitanyadesireddy1@gmail.com>
2021-10-19 12:15:15 +0530

    latest db

--- application/controllers/api/ApiDemoController.php
+++ application/controllers/api/ApiDemoController.php
@@ -2,8 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 require APPPATH.'libraries/RestController.php';
-
+require APPPATH.'libraries/Format.php';
 use chriskacerguis\RestServer\RestController;
+use chriskacerguis\RestServer\Format;
 class ApiDemoController extends RestController
 {
 	public function index_get()
---
+++ studentonlineexam (2).sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2021 at 08:43 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `studentonlineexam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers`
+--
+
+CREATE TABLE `answers` (
+  `id` int(11) NOT NULL,
+  `questionid` int(10) NOT NULL,
+  `answer` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `answers`
+--
+
+INSERT INTO `answers` (`id`, `questionid`, `answer`) VALUES
+(1, 2, ''),
+(2, 3, 'option1'),
+(3, 4, 'option1'),
+(4, 1, 'option1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `api_keys`
+--
+
+CREATE TABLE `api_keys` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `my_key` varchar(40) NOT NULL,
+  `level` int(2) NOT NULL,
+  `ignore_limits` tinyint(1) NOT NULL DEFAULT '0',
+  `is_private_key` tinyint(1) NOT NULL DEFAULT '0',
+  `ip_addresses` text,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `api_keys`
+--
+
+INSERT INTO `api_keys` (`id`, `user_id`, `my_key`, `level`, `ignore_limits`, `is_private_key`, `ip_addresses`, `date_created`) VALUES
+(1, 0, 'funda123', 0, 0, 0, NULL, '2021-10-19 06:28:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `sn` int(11) NOT NULL,
+  `question` varchar(200) NOT NULL,
+  `option1` varchar(100) NOT NULL,
+  `option2` varchar(100) NOT NULL,
+  `option3` varchar(100) NOT NULL,
+  `option4` varchar(100) NOT NULL,
+  `marks` int(10) NOT NULL,
+  `negativemarks` int(10) NOT NULL,
+  `answer` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`sn`, `question`, `option1`, `option2`, `option3`, `option4`, `marks`, `negativemarks`, `answer`) VALUES
+(1, 'what is the distance?', '9', '6', 'uy', 'jhg', 0, 0, ''),
+(2, 'what is the distance?', '9', '6', 'uy', 'jhg', 0, 0, ''),
+(3, 'what is the distance?', '9', '54', '65', 'yt', 54, 0, ''),
+(4, 'what is the distance?', '9', '54', '65', 'yt', 54, 0, ''),
+(5, 'what is the distance?', '9', '54', '65', 'jhg', 1, 1, 'option1'),
+(6, 'what is the distance?', '9', '54', '65', 'jhg', 1, 1, 'option1'),
+(7, 'what is the distance?', '9', '54', '65', 'jhg', 1, 1, 'option1'),
+(8, 'what is the distance?', '9', '54', '65', 'jhg', 1, 1, 'option1'),
+(9, 'what is the distance?', '9', '54', '65', 'jhg', 1, 1, 'option1'),
+(10, 'what is the distance?', '9', '54', '65', 'jhg', 1, 1, 'option1'),
+(11, 'what is the distance?', '9', '54', '65', 'jhg', 1, 1, 'option1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `id` int(20) NOT NULL,
+  `firstname` text NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phonenumber` int(20) NOT NULL,
+  `city` text NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `confirmpassword` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`id`, `firstname`, `lastname`, `email`, `phonenumber`, `city`, `password`, `confirmpassword`, `gender`) VALUES
+(1, 'Desireddy', 'Chaitanya', 'Chaitanyadesireddy1@gmail.com', 2147483647, 'Gudur', 'chaituidea1', 'chaituidea1', 'male'),
+(2, 'Desireddy', 'Chaitanya', 'Chaitanyadesireddy1@gmail.com', 2147483647, 'Gudur', '1d477633bcdfdbd100c8d34f6d105a24', 'chaituidea1', 'male');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signup`
+--
+
+CREATE TABLE `signup` (
+  `email` varchar(30) NOT NULL,
+  `tempkey` varchar(100) NOT NULL,
+  `expdate` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `api_keys`
+--
+ALTER TABLE `api_keys`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`sn`);
+
+--
+-- Indexes for table `register`
+--
+ALTER TABLE `register`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `api_keys`
+--
+ALTER TABLE `api_keys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `register`
+--
+ALTER TABLE `register`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
